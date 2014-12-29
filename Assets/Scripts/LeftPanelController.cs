e8a07b2b1053ffe467c9f0bc9f2e58edf46c49e2
Dominik <dominikpielak@gmail.com>
2014-12-29 18:17:53 +0100

    init
    wi

---
+++ Assets/Scripts/LeftPanelController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.Windows.Forms;
+
+public class LeftPanelController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+    void WczytajClick()
+    {
+        Stream myStream = null;
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+        openFileDialog1.InitialDirectory = "c:\\";
+        //openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+        //openFileDialog1.FilterIndex = 2;
+        //openFileDialog1.RestoreDirectory = true;
+
+        if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        {
+            try
+            {
+                myStream = openFileDialog1.OpenFile();
+                using (myStream)
+                {
+
+                    // Insert code to read the stream here.
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+            }
+        }
+    }
+
+    void ZapiszClick()
+    {
+        Stream myStream;
+        SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+        //saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+        //saveFileDialog1.FilterIndex = 2;
+        //saveFileDialog1.RestoreDirectory = true;
+
+        if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+        {
+            myStream = saveFileDialog1.OpenFile();
+            // Code to write the stream goes here.
+            myStream.Close();
+        }
+    }
+}
---
+++ Assets/Scripts/Wierzcholek.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wierzcholek : MonoBehaviour
+{
+    #region SCENE REFERENCES
+
+    public Transform Podswietlenie;
+    public Transform Rdzen;
+
+    #endregion
+
+    #region PREFAB REFERENCES
+
+    public Material Kolor;
+    public Material AktywnyKolor;
+    public Material NieAktywnyKolor;
+
+    #endregion
+
+    private bool isMarked;
+	// Use this for initialization
+	void Start ()
+	{
+	    isMarked = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void Zaznacz()
+    {
+        if (isMarked)
+        {
+            isMarked = false;
+            Podswietlenie.renderer.material = NieAktywnyKolor;
+        }
+        else
+        {
+            isMarked = true;
+            Podswietlenie.renderer.material = AktywnyKolor;
+        }
+    }
+}
